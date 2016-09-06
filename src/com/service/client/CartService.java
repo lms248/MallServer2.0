240dd4e991190d4e41c2c718d3ea55b04eb66d0f
lms248 <lms248@163.com>
2016-09-06 10:31:54 +0800

    add cart delete

--- src/com/service/client/CartService.java
+++ src/com/service/client/CartService.java
@@ -272,4 +272,118 @@ public class CartService {
 		out.flush();
 		out.close();
 	}
+	
+	/** 删除 */
+	@RequestMapping(value ="delete",method=RequestMethod.GET)
+	@ResponseBody
+	public void delete(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException{
+		response.setContentType("text/html;charset=utf-8");
+		request.setCharacterEncoding("utf-8");
+		response.setCharacterEncoding("utf-8");
+		PrintWriter out = response.getWriter();
+		
+		String token = request.getParameter("token");
+		String goodsId = request.getParameter("goodsId");
+		String tags = request.getParameter("tags");
+		
+		JSONObject obj = new JSONObject();
+		
+		UserBean user = UserDao.loadByToken(token);
+		if (user == null) {
+			obj.put("code", Def.CODE_FAIL);
+			obj.put("msg", "用户不存在");
+			out.print(obj);
+			
+			out.flush();
+			out.close();
+			return;
+		}
+		
+		CartBean cart = CartDao.loadByUid(user.getUid());
+		if (cart == null) {
+			obj.put("code", Def.CODE_FAIL);
+			obj.put("msg", "购物车为空");
+			out.print(obj);
+			
+			out.flush();
+			out.close();
+			return;
+		} else {
+			JSONArray goodsList1 = JSONArray.fromObject(cart.getGoodsList());
+			JSONArray goodsList2 = new JSONArray();
+			for (int i = 0; i < goodsList1.size(); i++) {
+				JSONObject obj2 = JSONObject.fromObject(goodsList1.get(i));
+				System.out.println("tags===="+tags);
+				System.out.println("JSON.parseObject(tags)===="+JSON.parseObject(tags));
+				System.out.println("obj2.get('tags').toString()===="+obj2.get("tags").toString());
+				if (obj2.get("goodsId") != null 
+						&& goodsId.equals(obj2.get("goodsId").toString())  
+						&& JSON.parseObject(tags).toString().equals(obj2.get("tags").toString())) {
+					break;
+				}
+				goodsList2.add(obj2);
+			}
+			
+			cart.setGoodsList(goodsList2.toString());
+			cart.setUpdateTime(System.currentTimeMillis());
+			CartDao.update(cart);
+		}
+		
+		JSONArray goodsList = JSONArray.fromObject(cart.getGoodsList());
+		Map<Long, JSONArray> goodsMap = new HashMap<Long, JSONArray>();
+		for (int i = 0; i < goodsList.size(); i++) {
+			JSONArray goodsArr = new JSONArray();
+			JSONObject goodsObj = JSONObject.fromObject(goodsList.get(i));
+			GoodsBean goods = GoodsDao.loadByGoodsId(goodsObj.getLong("goodsId"));
+			ShopBean shop = ShopDao.loadByShopId(goods.getShopId());
+			if (goodsMap.get(shop.getShopId()) == null) {
+				goodsArr.add(goodsObj);
+				goodsMap.put(shop.getShopId(), goodsArr);
+			} else {
+				goodsArr = goodsMap.get(shop.getShopId());
+				goodsArr.add(goodsObj);
+				goodsMap.put(shop.getShopId(), goodsArr);
+			}
+		}
+		
+		JSONArray arr = new JSONArray();
+		JSONObject obj2 = new JSONObject();
+		for (Map.Entry<Long, JSONArray> map : goodsMap.entrySet()) {
+			ShopBean shop = ShopDao.loadByShopId(map.getKey());
+			obj2 = new JSONObject();
+			obj2.put("shopId", shop.getShopId());
+			obj2.put("shopName", shop.getName());
+			obj2.put("shopImage", shop.getImage());
+			obj2.put("shopThumb", shop.getThumbnail());
+			JSONArray arr2 = new JSONArray();
+			for (int i = 0; i < map.getValue().size(); i++) {
+				JSONObject obj3 = JSONObject.fromObject(map.getValue().get(i));
+				JSONObject obj4 = JSONObject.fromObject(map.getValue().get(i));
+				GoodsBean goods = GoodsDao.loadByGoodsId(obj3.getLong("goodsId"));
+				obj4.put("goodsId", obj3.get("goodsId"));
+				obj4.put("goodsName", goods.getName());
+				obj4.put("goodsLogo", goods.getLogo());
+				obj4.put("goodsLogoThumb", goods.getLogoThumb());
+				obj4.put("prePrice", goods.getPrePrice());
+				obj4.put("curPrice", goods.getCurPrice());
+				obj4.put("amount", obj3.get("amount"));
+				obj4.put("tags", obj3.get("tags"));
+				arr2.add(obj4);
+			}
+			obj2.put("goodsList", arr2);
+			arr.add(obj2);
+		}
+		
+		obj.put("code", Def.CODE_SUCCESS);
+		obj.put("msg", "购物车列表");
+		obj.put("data", arr);
+		out.print(obj);
+		
+		System.out.println(obj);
+		
+		out.flush();
+		out.close();
+	}
 }
+
