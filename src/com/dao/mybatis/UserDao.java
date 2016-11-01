a4bfa97a3b1b123fceb6e99be1d9f32327252b0c
lms248 <lms248@163.com>
2016-11-01 16:57:56 +0800

    spring集成配置mybatis

--- src/com/dao/mybatis/UserDao.java
+++ src/com/dao/mybatis/UserDao.java
@@ -2,13 +2,23 @@ package dao.mybatis;
 
 import java.util.List;
 
+import org.springframework.stereotype.Repository;
+
 import bean.client.UserBean;
 
+@Repository
 public interface UserDao {
 	
-	//public List<UserBean> loadById(int id) throws Exception; 
 	public UserBean loadById(int id); 
+	
+	public UserBean loadByUid(String uid); 
+	
+	public UserBean loadByUsername(String username); 
+	
+	public UserBean loadByToken(String token); 
 	
 	public List<UserBean> loadAllUser(int index, int size); 
 	
+	public int save(UserBean bean);
+	public int update(UserBean bean);
 }
